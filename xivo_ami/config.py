4d8b4b15800ef61c26868868b9c01f61307ef504
Cedric Abunar <cabunar@avencall.com>
2014-09-07 21:24:52 -0400

    refactor config class

--- xivo_ami/config.py
+++ xivo_ami/config.py
@@ -16,12 +16,14 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 import argparse
+import pprint
 import os
 import yaml
 
 from xivo_bus.ctl.config import BusConfig
 
 _DAEMONNAME = 'xivo-amid'
+_CONF_DIR = '/etc/xivo/xivo-amid'
 _CONF_FILENAME = '{}.yml'.format(_DAEMONNAME)
 
 
@@ -31,14 +33,30 @@ class ConfigXivoAMId(object):
     _PID_FILENAME = '/var/run/{daemon}/{daemon}.pid'.format(daemon=_DAEMONNAME)
     _SOCKET_FILENAME = '/tmp/{}.sock'.format(_DAEMONNAME)
 
-    def __init__(self, adict):
-        self._update_config(adict)
+    def __init__(self, d):
+        for k in d:
+            if isinstance(d[k], dict):
+                self.__dict__[k] = ConfigXivoAMId(d[k])
+            elif isinstance(d[k], (list, tuple)):
+                l = []
+                for v in d[k]:
+                    if isinstance(v, dict):
+                        l.append(ConfigXivoAMId(v))
+                    else:
+                        l.append(v)
+                self.__dict__[k] = l
+            else:
+                self.__dict__[k] = d[k]
 
-    def _update_config(self, adict):
-        self.__dict__.update(adict)
-        for k, v in adict.items():
-            if isinstance(v, dict):
-                self.__dict__[k] = ConfigXivoAMId(v)
+    def __getitem__(self, name):
+        if name in self.__dict__:
+            return self.__dict__[name]
+
+    def __iter__(self):
+        return iter(self.__dict__.keys())
+
+    def __repr__(self):
+        return pprint.pformat(self.__dict__)
 
     @property
     def ajam_url(self):
@@ -54,7 +72,7 @@ class ConfigXivoAMId(object):
         return bus_config_obj
 
 
-def configure():
+def get_arg_parsed():
     parser = argparse.ArgumentParser()
     parser.add_argument('-f',
                         '--foreground',
@@ -71,9 +89,9 @@ def configure():
                         action='store',
                         help="The owner of the process.")
     parser.add_argument('-c',
-                        '--config_path',
+                        '--config-path',
                         action='store',
-                        default="/etc/xivo/xivo-amid",
+                        default=_CONF_DIR,
                         help="The path where is the config file. Default: %(default)s")
     parser.add_argument('--disable-bus',
                         action='store_true',
@@ -82,11 +100,12 @@ def configure():
     return parser.parse_args()
 
 
-def _get_config_raw(config_path):
+def _get_config_dict(config_path):
     path = os.path.join(config_path, _CONF_FILENAME)
     with open(path) as fobj:
         return yaml.load(fobj)
 
-args_parsed = configure()
-config = ConfigXivoAMId(_get_config_raw(args_parsed.config_path))
-config._update_config(vars(args_parsed))
+args_parsed = get_arg_parsed()
+data = _get_config_dict(args_parsed.config_path)
+data.update(vars(args_parsed))
+config = ConfigXivoAMId(data)
