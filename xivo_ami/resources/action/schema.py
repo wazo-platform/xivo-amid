75e2b486e582de9a5f60bfe64ec6bc16a0a28960
Mehdi Abaakouk <mabaakouk@wazo.io>
2019-08-08 11:49:51 +0200

    marshmallow 3.X code update

--- xivo_ami/resources/action/schema.py
+++ xivo_ami/resources/action/schema.py
@@ -1,11 +1,11 @@
-# Copyright (C) 2016 Avencall
+# Copyright 2016-2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from marshmallow import fields, Schema
-from marshmallow.validate import Length
 
 
 class Command(Schema):
     command = fields.String(required=True)
 
-command_schema = Command(strict=True)
+
+command_schema = Command()
