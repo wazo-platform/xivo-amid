f37cbde07864710eb4774005dd7a5335c89750d2
Francois Blackburn <fblackburn@wazo.community>
2019-09-09 14:50:13 -0400

    rename setup.py and executable

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-# Copyright 2013-2018 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2013-2019 The Wazo Authors  (see the AUTHORS file)
 
 from setuptools import setup
 from setuptools import find_packages
 
 setup(
-    name='xivo-amid',
+    name='wazo-amid',
     version='0.1',
-    description='XiVO AMI adapter server',
+    description='Wazo AMI daemon',
     author='Wazo Authors',
     author_email='dev@wazo.community',
     url='http://wazo.community',
@@ -15,7 +15,7 @@ setup(
 
     packages=find_packages(),
     package_data={
-        'xivo_ami.resources.api': ['*.yml'],
+        'wazo_amid.resources.api': ['*.yml'],
     },
-    scripts=['bin/xivo-amid'],
+    scripts=['bin/wazo-amid'],
 )
