72b42fe335619f2526d1f831dbbb6ad55c4b5f4d
Francois Blackburn <fblackburn@wazo.community>
2022-02-22 09:23:54 -0500

    use marshmallow helpers from lib-python

--- wazo_amid/resources/action/schema.py
+++ wazo_amid/resources/action/schema.py
@@ -1,7 +1,8 @@
 # Copyright 2016-2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from marshmallow import fields, Schema
+from xivo.mallow import fields
+from xivo.mallow_helpers import Schema
 
 
 class Command(Schema):
